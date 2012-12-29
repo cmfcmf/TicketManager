5b94fbb89c06e7c2997e12f9e872942f180e05ea
cmfcmf <cmfcmf.flach@gmail.com>
2012-12-29 16:25:06 +0100

    Added (dummy) user controller

---
+++ lib/TicketManager/Controller/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright Christian Flach
+ */
+class TicketManager_Controller_User extends Zikula_AbstractController
+{
+	public function main()
+	{
+		echo "Dummy function";
+		return true;
+	}
+}
