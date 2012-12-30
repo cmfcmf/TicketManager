01bcb648475b345740418753586c92e0acf58730
cmfcmf <cmfcmf.flach@gmail.com>
2012-12-30 14:45:20 +0100

    Updated ticket generating test-function

--- lib/TicketManager/Controller/Admin.php
+++ lib/TicketManager/Controller/Admin.php
@@ -85,15 +85,16 @@ class TicketManager_Controller_Admin extends Zikula_AbstractController
 	public function test()
 	{
 		$return = ModUtil::apiFunc($this->name, 'Ticket', 'reserve',
-			array('number' => 2,
+			array(
+				'number' => 4,
 				'eventname' =>'Testevent',
-				'picture' => null, //Path to big picture
+				'picture' => 'images/logo.gif', //Path to big picture
 				'module' => ModUtil::getIdFromName($this->name),
-				'logo' => null, //Path to little logo
+				'logo' => 'images/zk-power.png', //Path to little logo
 				'price' => 4,
-				'information' => array('uid' => 373),
+				'information' => array('uid' => 373, 'foo' => 'bar'),
 				'startdate' => '15.3.2013 12:00',
-				'shortdescription' => 'Lorem ipsum latinum dolorem sint sant sunt.'
+				'shortdescription' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam'
 			)
 		);
 
