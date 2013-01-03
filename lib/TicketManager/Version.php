6a104446b109e508fe70f7dd95356d9051e21d2f
cmfcmf <cmfcmf.flach@gmail.com>
2013-01-03 15:30:17 +0100

    Updated version

--- lib/TicketManager/Version.php
+++ lib/TicketManager/Version.php
@@ -19,12 +19,12 @@ class TicketManager_Version extends Zikula_AbstractVersion
 		$modversion['name']           = 'TicketManager';
 		$modversion['displayname']    = 'TicketManager';
 		$modversion['description']    = $this->__('Manage your TICKETS easy!');
-		$modversion['version']        = '0.0.4';
+		$modversion['version']        = '0.0.5';
 		$modversion['official']       = false;
 		$modversion['author']         = 'Christian Flach';
 		$modversion['contact']        = 'cmfcmf.flach@gmail.com';
 		$modversion['admin']          = true;
-		$modversion['user']           = true;
+		$modversion['user']           = false;
 		$modversion['core_min'] = '1.3.0';
 		$modversion['core_max'] = '1.3.99';
 
